6fb6e387dccb15cdb6e1f90d7698016165cf1292
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-06-07 18:29:11 +0100

    added task 3

---
+++ tasks/task-3.js
@@ -0,0 +1,46 @@
+/*
+  TASK 3
+
+
+
+  PART 1:
+
+  You have been given an array of pets with their species and names. Each species should be a key on
+  the object and the value of the key should be an array of all the pet names. For example:
+
+  {
+    parrot: ['Fred', 'Jasper'],
+    snake: ['Luke']
+  }
+
+  Log the full object.
+
+
+  PART 2:
+
+  Add an additional additional dog to the object. You may name this dog whatever you like. Log the
+  whole object.
+
+
+  PART 3:
+
+  Add another pet of a new species to the object. You may choose the species and the pet name. Log
+  the whole object.
+
+*/
+
+const animals = [
+  ['dog', 'Bertie'],
+  ['dog', 'Mavis'],
+  ['cat', 'Choco'],
+  ['dog', 'Budge'],
+  ['cat', 'Whisk'],
+  ['cat', 'Odin'],
+  ['dog', 'Clive'],
+  ['hamster', 'Tyranosaur'],
+  ['dog', 'Arthur'],
+  ['hamster', 'Munch Munch'],
+  ['cat', 'Data'],
+];
+
+// WRITE YOUR CODE BELOW THIS LINE
