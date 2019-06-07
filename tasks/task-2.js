aa4817bdf153409f4c87470f0cdb1a81831008d9
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-06-07 18:13:44 +0100

    added the second task

---
+++ tasks/task-2.js
@@ -0,0 +1,22 @@
+/*
+  TASK 2
+
+
+
+  PART 1:
+
+  Calculate the sum of all the numbers in the array. Log your answer.
+
+
+  PART 2:
+
+  Find the most commonly occurring number. Log your answer.
+
+
+  Part 3:
+
+  Calculate the sum of only the ODD numbers in the sequence. Log your answer.
+
+*/
+
+const numbers = [5, 3, 8, 1, 2, 4, 9, 2, 8, 7, 6, 5, 2, 0, 2];
