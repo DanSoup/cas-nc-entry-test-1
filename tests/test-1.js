3d5e9312e6017154867acfcc2d2c0dfe1d4d9662
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-06-07 18:01:38 +0100

    added additional parts to task 1

---
+++ tests/test-1.js
@@ -0,0 +1,43 @@
+/*
+  TEST 1
+
+    Take this array of names and fix the capitalization on them. 
+
+
+  PART 1: 
+    
+    Create a NEW array that has all the names with the correct capitalization. Each name
+    should have the first letter be in UPPERCASE and the rest of the letters in LOWERCASE.
+
+    susan => Susan
+    vaneSSa => Vanessa
+    
+    Make sure you console.log your new array.
+
+
+  PART 2:
+    
+    Create another NEW array that only contains names that begin with an "S". You should use
+    the array you made in PART 1 so the names have the correct capitalization. Make sure you
+    console.log your new array.
+
+
+  PART 3:
+  
+    Create another NEW array that has all the names from the original array but backwards.
+
+    Daniel => leinaD
+    ROSS => SSOR
+
+    For this part capitalization is not important. Make sure you console.log your new array.
+
+    
+*/
+
+const names = [
+  'susan', 'keith', 'eli', 'sebastian', 'daniel',
+  'STEPHEN', 'MitCh', 'dAVID', 'vaneSSa', 'ROSS'
+]
+
+// WRITE YOUR CODE BELOW THIS LINE
+
